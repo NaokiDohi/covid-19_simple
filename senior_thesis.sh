9698ffd4fd41d705e866b3e203fbd89fc1b29755
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2020-11-23 17:29:29 +0900

    .sh was rewrited.

--- senior_thesis.sh
+++ senior_thesis.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
-for n in 16 32 64 128 256 512 1024
+for n in 16 #32 64 128 256 512 1024
 do
-    for dropout in 0 0.2 0.4 0.5 0.6
+    for dropout in 0 #0.2 0.4 0.5 0.6
     do
-        python3 ./gru/main.py $n $dropout > log_`date "+%Y%m%d_%H%M%S"`.txt
+        python3 ./covid_19_forecast_with_lstm.py $n $dropout > ./lstm/log/log_"$n"_"$dropout".txt
     done
 done
 # $n $dropout
+# log_` date "+%Y%m%d_%H%M%S"`.txt
