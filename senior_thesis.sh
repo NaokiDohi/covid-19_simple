ff695433ff2a2d10fc940ecd905a128a0ac57d52
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2020-11-25 14:30:46 +0900

    sh was fixed.

--- senior_thesis.sh
+++ senior_thesis.sh
@@ -3,7 +3,7 @@ mkdir -p mlp/log rnn/log lstm/log gru/log mlp/model rnn/model lstm/model gru/mod
 
 for n in 16 32 64 128 256 512 1024
 do
-    for dropout in 0 0.2 0.4 0.5 0.6
+    for dropout in 0.0 0.2 0.4 0.5 0.6
     do
         # python3 covid_19_forecast_with_mlp.py $n $dropout > lstm/log/log_"$n"_"$dropout".txt
         # python3 covid_19_forecast_with_rnn.py $n $dropout > lstm/log/log_"$n"_"$dropout".txt
